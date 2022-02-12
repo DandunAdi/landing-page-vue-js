00d5fee3db8860c64ee7fc1bed3fa8b1ce9c3759
Dandun Adi <dandun.adi@gmail.com>
2022-02-12 15:18:30 +0700

    setup font

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,15 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;700&display=swap');
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+body {
+  font-family: 'Mulish', sans-serif;
 }
 </style>
