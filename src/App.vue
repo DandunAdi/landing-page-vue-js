5091247fca37f60be3dc3e831aa5746dbca9bcd2
Dandun Adi <dandun.adi@gmail.com>
2022-02-12 15:08:34 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Hello world</h1>
+  </main>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
